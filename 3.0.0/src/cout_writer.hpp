138d53c47727ab1f5f48f68062ee0f26125aa066
Maurits Lam <mhjlam@gmail.com>
2025-08-14 01:51:26 +0200

    MCML 3.0 WIP

---
+++ 3.0.0/src/cout_writer.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "writer.hpp"
+
+#include <memory>
+#include <ostream>
+
+
+class CoutWriter : public Writer
+{
+public:
+    CoutWriter() : Writer()
+    {
+        m_output.reset(&std::cout);
+    }
+    ~CoutWriter()
+    {
+        m_output.release();
+    }
+};
